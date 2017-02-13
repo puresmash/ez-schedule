a464c57eb86b825659fac836f87724d3ad5180b6
swat <puresmash@gmail.com>
2017-02-14 04:16:57 +0800

    Add babel-plugin-transform-flow-strip-types
    1. Add babel-plugin-transform-flow-strip-types.

--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -1,53 +1,58 @@
 
 // "use strict";
 
-var ExtractTextPlugin = require('extract-text-webpack-plugin');
-var extractCSS = new ExtractTextPlugin('./css/[name].css');
-var autoprefixer = require('autoprefixer');
-var webpack = require('webpack');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+const extractCSS = new ExtractTextPlugin('./css/[name].css');
+const autoprefixer = require('autoprefixer');
+const webpack = require('webpack');
 
 module.exports = {
-    entry: ['babel-polyfill', './js/entry.js'],
-    output:{
-        path: "./public/assets",
-        filename: "bundle.min.js",
-        publicPath: '/public/'
-    },
-    module: {
-        loaders: [
-            {
-              test: /\.jsx?$/,
-              exclude: /(node_modules|bower_components)/,
-              loader: 'babel',
-              query: {
-                presets: ['react', 'es2015'],
-                plugins: ['transform-object-rest-spread', 'transform-class-properties']
-              }
-            },
-            {
-              test: /\.scss$/,
-              loader: ExtractTextPlugin.extract('style', ['css', 'postcss', 'sass'])
-            },
-            {
-              test: /\.css$/,
-              loader: "style!css"
-            }
-        ]
-    },
-    postcss: [
-        autoprefixer({browsers: ['last 5 versions']})
+  entry: ['babel-polyfill', './js/entry.js'],
+  output: {
+    path: './public/assets',
+    filename: 'bundle.min.js',
+    publicPath: '/public/',
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: 'babel',
+        query: {
+          presets: ['react', 'es2015'],
+          plugins: [
+            'transform-object-rest-spread',
+            'transform-class-properties',
+            'transform-flow-strip-types',
+          ],
+        },
+      },
+      {
+        test: /\.scss$/,
+        loader: ExtractTextPlugin.extract('style', ['css', 'postcss', 'sass']),
+      },
+      {
+        test: /\.css$/,
+        loader: 'style!css',
+      },
     ],
-    plugins: [
-        new webpack.DefinePlugin({
-            "process.env": {
-                NODE_ENV: JSON.stringify('production')
-            }
-        }),
-        new webpack.optimize.UglifyJsPlugin({
-            compress: {
-                warnings: false,
-            }
-        }),
-        extractCSS
-    ]
+  },
+  postcss: [
+    autoprefixer({ browsers: ['last 5 versions'] }),
+  ],
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: JSON.stringify('production'),
+      },
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+      compress: {
+        warnings: false,
+      },
+    }),
+    extractCSS,
+  ]
 };
